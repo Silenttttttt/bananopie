85e7eed076d8b7fa8586720b76414e57360653cc
Muni Besen <munibesen@gmail.com>
2023-05-24 14:57:48 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="John Doe",
     author_email="prussia@prussia.dev",
     packages=["bananopie"],
-    install_requires=["requests", "ed25519_blake2b"],
+    install_requires=["requests", "ed25519_blake2b", "nanopy"],
     version="0.1.0",
     license="MIT",
     description="A python library to simplify sending and receiving Banano. Also a RPC wrapper.",
